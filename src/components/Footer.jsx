9b38ad72e65dec061133b4bb3e6d769f3fe1e74e
Justin <justinmanfung@gmail.com>
2026-06-14 02:59:10 -0400

    Fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,10 @@ export const Footer = () => {
     <footer className="py-8 px-4 bg-card relative border-t 
     border-border mt-12 pt-8 flex flex-wrap justify-between items-center"
     >
+      <p className="text-sm text-muted-foreground">
+        {" "}
+        &copy; {new Date().getFullYear()} Justin Fung. All rights reserved.
+      </p>
       <a
         href="#hero"
         className="p-2 rounded-full bg-primary/10 hover:bg-primary/20 text-primary 
